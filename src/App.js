d66ebb748d71eabae15b1b18e914c395c5605933
Mrtgk93 <mrtgk93@hotmail.com>
2023-02-05 22:28:30 +0300

    ödev teslimi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Karakter from "./components/Karakter";
+import { response } from "msw";
 
 const App = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then((response) => {
+        setData(response.data.results);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <h1 className="Header">Star Wars Major Characters List </h1>
+      <Karakter data={data[0]} />
+      <Karakter data={data[1]} />
+      <Karakter data={data[2]} />
+      <Karakter data={data[3]} />
+      <Karakter data={data[4]} />
+      <Karakter data={data[5]} />
+      <Karakter data={data[6]} />
+      <Karakter data={data[7]} />
+      <Karakter data={data[8]} />
+      <Karakter data={data[9]} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,54 @@
 // Karakter bileşeniniz buraya gelecek
+import React, { useState } from "react";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from "reactstrap";
+
+const stil = {
+  width: "500px",
+};
+
+function Karakter(props) {
+  const { data } = props;
+  const Skarakter = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
+
+  const [open, setOpen] = useState("0");
+  const toggle = (id) => {
+    if (open === id) {
+      setOpen();
+    } else {
+      setOpen(id);
+    }
+  };
+
+  return (
+    <Skarakter>
+      <Accordion style={stil} open={open} toggle={toggle}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">
+            <h3>{data.name}</h3>
+          </AccordionHeader>
+          <AccordionBody accordionId="1">
+            <li>{data.gender}</li>
+            <li>{data.height}</li>
+            <li>{data.mass}</li>
+            <li>{data.birth_year}</li>
+            <li>{data.eye_color}</li>
+            <li>{data.hair_color}</li>
+            <li>{data.skin_color}</li>
+          </AccordionBody>
+        </AccordionItem>
+      </Accordion>
+    </Skarakter>
+  );
+}
+export default Karakter;
